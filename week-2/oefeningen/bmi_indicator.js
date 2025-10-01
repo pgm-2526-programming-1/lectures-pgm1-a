dc951991aeaa61a48840df9f39e966bf0eb5e228
Michael Vanderpoorten <michael@yungpanda.com>
2025-10-01 12:10:40 +0200

    Oefeningen week 2

---
+++ week-2/oefeningen/bmi_indicator.js
@@ -0,0 +1,17 @@
+const length = 1.72;
+const weight = 100;
+
+const bmi = Math.floor(weight / (length * length));
+
+let text = `Je hebt een bmi van ${bmi}.`;
+if (bmi <= 18.5) {
+  text += " Je hebt ondergewicht";
+} else if (bmi < 25) {
+  text += " Je hebt het aanbevolen gewicht";
+} else if (bmi <= 30) {
+  text += " Je hebt overgewicht";
+} else {
+  text += " Je bent zwaarlijvig";
+}
+
+console.log(text);
